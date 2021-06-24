4a5b17c667abdc7fedfa376c7a5c7b0876a99962
Jason Guiditta <jguiditt@redhat.com>
2021-06-24 10:16:41 -0400

    Set up better development and release config.
    This patch preps us for pulling in the initial code extraction
    from the original (embedded) code. It attempts to make it easy
    both for developers getting set up, and for releasing to pypi.
    
    Signed-off-by: Jason Guiditta <jguiditt@redhat.com>

--- setup.py
+++ setup.py
@@ -1,36 +1,5 @@
-"""
-flask-container-scaffold
--------------
+#!/usr/bin/env python
 
-A library to facilitate deploying flask applications easily in either a
-container environment, development, or another production configuration
-"""
 from setuptools import setup
 
-
-setup(
-    name='flask-container-scaffold',
-    version='1.0',
-    url='http://github.com/release-depot/flask-container-scaffold',
-    license='MIT',
-    author='Jason Guiditta',
-    author_email='jason.guiditta@gmail.com',
-    description='Configuration layer to aid in deployment of Flask apps',
-    long_description=__doc__,
-    py_modules=['flask_container_scaffold'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    install_requires=[
-        'Flask'
-    ],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
-)
+setup()
