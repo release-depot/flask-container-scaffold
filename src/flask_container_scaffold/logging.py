5705ef8af49069a37ec3dbdd5929fc4d5b4bce57
Julie Pichon <jpichon@redhat.com>
2021-08-06 16:45:10 +0100

    Add logging formatter utility that extracts the IP from requests

---
+++ src/flask_container_scaffold/logging.py
@@ -0,0 +1,50 @@
+import logging
+
+from flask import has_request_context, request
+
+
+class FlaskRequestFormatter(logging.Formatter):
+    """
+    A Formatter logging class to add IP information to the log records.
+
+    Usage example:
+
+    from flask_container_scaffold.logging import FlaskRequestFormatter
+
+    dictConfig({
+        'version': 1,
+        'formatters': {
+            'default': {
+                '()': FlaskRequestFormatter,
+                'format': '[%(asctime)s] %(remote_addr)s %(levelname)s: %(message)s',
+            },
+        },
+        ...
+    })
+    """
+
+    def get_ip_from_forwarded(self, field):
+        # RFC 7239 defines the following format for the Forwarded field:
+        # for=12.34.56.78;host=example.com;proto=https, for=23.45.67.89
+        # In testing, the first IP has consistently been the real user IP.
+        forwarded = field.split(",")[0]
+        for value in forwarded.split(";"):
+            if value.startswith("for="):
+                return value.split("=")[1]
+        else:
+            return None
+
+    def format(self, record):
+        if has_request_context():
+            # HTTP_FORWARDED seems to be the most reliable way to get the
+            # user real IP.
+            forwarded = request.environ.get('HTTP_FORWARDED')
+            if forwarded:
+                ip = self.get_ip_from_forwarded(forwarded)
+                record.remote_addr = ip or request.remote_addr
+            else:
+                record.remote_addr = request.remote_addr
+        else:
+            record.remote_addr = "-"
+
+        return super().format(record)
---
+++ tests/unit/test_logging.py
@@ -0,0 +1,84 @@
+from logging import LogRecord
+
+from flask import Flask
+
+from flask_container_scaffold.logging import FlaskRequestFormatter
+
+
+FORMAT = "[%(remote_addr)s] %(msg)s"
+
+
+def test_request_formatter_no_context():
+    """
+    GIVEN a properly initialised FlaskRequestFormatter
+    WHEN it is called without a request context
+    THEN the log message contains "-" instead of an IP
+    """
+    record = LogRecord('', 1, '', 1, 'Test message', '', None)
+
+    formatter = FlaskRequestFormatter(FORMAT)
+
+    assert formatter.format(record) == "[-] Test message"
+
+
+def test_request_formatter_with_no_forwarded_ip():
+    """
+    GIVEN a properly initialised FlaskRequestFormatter
+    WHEN it is called within a request context
+    AND HTTP_FORWARDED isn't set
+    THEN the log message contains the remote_addr IP
+    """
+    record = LogRecord('', 1, '', 1, 'Test message', '', None)
+
+    with Flask("test").test_request_context(
+            environ_base=(("REMOTE_ADDR", "1.2.3.4"),)):
+        formatter = FlaskRequestFormatter(FORMAT)
+        assert formatter.format(record) == "[1.2.3.4] Test message"
+
+
+def test_request_formatter_with_forwarded_ip():
+    """
+    GIVEN a properly initialised FlaskRequestFormatter
+    WHEN it is called within a request context
+    AND HTTP_FORWARDED exists
+    THEN the log message contains the forwarded IP
+    """
+    env = (("REMOTE_ADDR", "1.2.3.4"),
+           ("HTTP_FORWARDED", "for=10.10.10.10;host=example.com;proto=http"),)
+    record = LogRecord('', 1, '', 1, 'Test message', '', None)
+
+    with Flask("test").test_request_context(environ_base=env):
+        formatter = FlaskRequestFormatter(FORMAT)
+        assert formatter.format(record) == "[10.10.10.10] Test message"
+
+
+def test_request_formatter_with_several_forwarded_ips():
+    """
+    GIVEN a properly initialised FlaskRequestFormatter
+    WHEN it is called within a request context
+    AND HTTP_FORWARDED contains several IPs
+    THEN the log message contains the first forwarded IP
+    """
+    env = (("REMOTE_ADDR", "1.2.3.4"),
+           ("HTTP_FORWARDED", "for=10.10.10.10;host=example.com, for=1.1.1.1"))
+    record = LogRecord('', 1, '', 1, 'Test message', '', None)
+
+    with Flask("test").test_request_context(environ_base=env):
+        formatter = FlaskRequestFormatter(FORMAT)
+        assert formatter.format(record) == "[10.10.10.10] Test message"
+
+
+def test_request_formatter_with_malformed_forwarded_field():
+    """
+    GIVEN a properly initialised FlaskRequestFormatter
+    WHEN it is called within a request context
+    AND HTTP_FORWARDED isn't the right format
+    THEN the log message contains the remote address
+    """
+    env = (("REMOTE_ADDR", "1.2.3.4"),
+           ("HTTP_FORWARDED", "host"),)
+    record = LogRecord('', 1, '', 1, 'Test message', '', None)
+
+    with Flask("test").test_request_context(environ_base=env):
+        formatter = FlaskRequestFormatter(FORMAT)
+        assert formatter.format(record) == "[1.2.3.4] Test message"
